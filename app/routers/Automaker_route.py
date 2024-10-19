df5238a79c85fb79882373f5f54c1ad782b3ef46
Meireles <antoniomeireles150@gmail.com>
2024-10-18 22:11:09 -0300

    Formulário de criação de montadoras

--- app/routers/Automaker_route.py
+++ app/routers/Automaker_route.py
@@ -1,20 +1,21 @@
-from typing import List
-
-from fastapi import APIRouter, Depends, Request
+from typing import Annotated
+from fastapi import APIRouter, Depends, Form, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
+from app.core.config import templates
 from app.core.database import get_db
 from app.crud import automaker_crud
 from app.models.vehicle_model import VehicleModel
 from app.schemas import automaker_schema
-from app.core.config import templates
 
 router = APIRouter()
 
 
-@router.post("/automakers", response_model=automaker_schema.AutomakerCreate)
+@router.post("/automakers")
 def create_automaker(
-    automaker: automaker_schema.AutomakerCreate, db: Session = Depends(get_db)
+    _input: Annotated[automaker_schema.AutomakerCreate, Form()],
+    db: Session = Depends(get_db),
 ):
     """
     Create a new automaker entry in the database.
@@ -26,7 +27,17 @@ def create_automaker(
     Returns:
         The created automaker entry.
     """
-    return automaker_crud.create_automaker(db, automaker)
+    try:
+        new_automaker = automaker_schema.AutomakerCreate(
+            nome=_input.nome, pais=_input.pais, ano_fundacao=_input.ano_fundacao
+        )
+
+        automaker_crud.create_automaker(db, new_automaker)
+
+        return RedirectResponse(url="/automakers", status_code=303)
+
+    except Exception as e:
+        return HTTPException(400, detail=str(e))
 
 
 @router.get("/automakers")
@@ -47,3 +58,8 @@ def get_automakers(request: Request, db: Session = Depends(get_db)):
         name="automakers_list.html",
         context={"montadoras": automakers},
     )
+
+
+@router.get("/automakers/create")
+def get_automaker(request: Request):
+    return templates.TemplateResponse(name="automaker_create.html", request=request)
